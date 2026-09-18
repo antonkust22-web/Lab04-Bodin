05ade0c030b2cf4b50b89182a707af3c06cbc0cc
antonkust22-web <antonkust22@icloud.com>
2026-09-18 19:35:21 +0300

    Lab04: if and else, age categories

--- Program.cs
+++ Program.cs
@@ -4,9 +4,26 @@ namespace Lab04_Bodin
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            //int age = 15;
+
+            //if (age >= 21) 
+            //{
+            //Console.WriteLine("Доступ разрешен");
+            //}
+            //Console.WriteLine("Программа продолжает работу");
+            
+            int age = 15;
+            if (age >= 18) {
+                Console.WriteLine("Доступ разрешен");
+            } else {
+                Console.WriteLine("Доступ запрещен");
+                Console.WriteLine($"Осталось ждать до совершеннолетия: {18 - age} лет");
+            }
+
+
         }
     }
 }
