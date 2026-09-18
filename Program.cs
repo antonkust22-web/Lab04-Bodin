0d0f8596c3b5e993f6268f8794ee1ad805529705
antonkust22-web <antonkust22@icloud.com>
2026-09-18 19:53:03 +0300

    Lab04: Logical Operators, Practice

--- Program.cs
+++ Program.cs
@@ -15,15 +15,33 @@ namespace Lab04_Bodin
             //}
             //Console.WriteLine("Программа продолжает работу");
             
-            int age = 15;
-            if (age >= 18) {
-                Console.WriteLine("Доступ разрешен");
+            //int age = 15;
+            //if (age >= 18) {
+            //    Console.WriteLine("Доступ разрешен");
+            //} else {
+            //    Console.WriteLine("Доступ запрещен");
+            //    Console.WriteLine($"Осталось ждать до совершеннолетия: {18 - age} лет");
+            //}
+
+            //int age = 61;
+            //if (age < 13) {
+            //    Console.WriteLine("Ребенок");
+            //} else if (age < 18) {
+            //    Console.WriteLine("Подросток");
+            //} else if (age < 60) {
+            //    Console.WriteLine("Взрослый");
+            //} else {
+            //    Console.WriteLine("Пенсионер");
+            //}
+
+            int age = 16;
+            double height = 1.4;
+            bool Soprovod = true;
+            if (age >= 14 && height >= 1.5 || age >= 14 && Soprovod == true) {
+                Console.WriteLine("Можно кататься");
             } else {
-                Console.WriteLine("Доступ запрещен");
-                Console.WriteLine($"Осталось ждать до совершеннолетия: {18 - age} лет");
+                Console.WriteLine("Пока нельзя");
             }
-
-
         }
     }
 }
