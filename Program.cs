9ca3a55bb6cb8113d2a71f34c67d18606c773421
Kadin <kadin@MacBook-Pro-Kadin.local>
2026-09-18 18:49:00 +0300

    Lab04: Logical Operators, Practice

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab04_Bodin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
